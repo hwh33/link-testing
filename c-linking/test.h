7b583e8268e519ee55baaf867931cf9aa5e713c5
Harry Harpham <harry@getlantern.org>
2021-06-25 14:59:04 -0600

    Add linking demo
    Run:
      cd c-linking
      gcc -fPIC -shared -o libtest.so test.c
      go run .
    
    From https://www.programmersought.com/article/7053200794

---
+++ c-linking/test.h
@@ -0,0 +1,12 @@
+#ifndef __TEST_H 
+#define __TEST_H 
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern int Value; 
+extern void sayHello();
+extern int add(int a, int b);
+#ifdef __cplusplus 
+} 
+#endif
+#endif
